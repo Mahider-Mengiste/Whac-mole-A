0c1a3f55b2fb2a4d2e7361f4cc03f6c847a486b2
mmengi18 <mmengi18@montgomerycollege.edu>
2022-06-22 19:27:08 -0400

    75% finished css and html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // grab the div element with the class .grid
 const grid  = document.querySelector(".grid")
-console.log(grid)
+
 // inside the grid create 9 more divs to create a 3x3 grid
       // createElement and append it to the parent div
       // loop 9 times to have the div's
@@ -15,6 +15,30 @@ cell.setAttribute("id", i);
 
  }  
 
-cell.
+ const displayMole = ()=> {
+      //  grab .cell 
+      // add a style to that element
+      const cells = document.querySelector('.cell')
+      // create a forEach loop to loop through each cell
+      console.log(cells)
+      cells.style.backgroundColor = '#a2836e';
+
+
+ }
+displayMole()
+
+const createStartButton = ()=>{
+      // create an element called start
+      // add a class .class
+
+      const start = document.createElement('button')
+      start.classList.add("start")
+      start.innerText = "Start"
+      console.log(start)
+      document.body.appendChild(start)
+  
+}
+
+createStartButton()
 
 
